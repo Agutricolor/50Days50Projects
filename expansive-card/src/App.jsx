f70671e3983904f1cba8eefbc3b0e66a428c5fb9
Agustin Vazquez <agustinvsexto196@gmail.com>
2022-10-04 19:01:00 -0300

    expansive-card

--- expansive-card/src/App.jsx
+++ expansive-card/src/App.jsx
@@ -2,34 +2,88 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [active, setActive] = useState("");
+  const handleActive = (e) => {
+    if (e.currentTarget.className.split(" ")[0] == "uno") setActive("uno");
+    if (e.currentTarget.className.split(" ")[0] == "dos") setActive("dos");
+    if (e.currentTarget.className.split(" ")[0] == "tres") setActive("tres");
+    if (e.currentTarget.className.split(" ")[0] == "cuatro")
+      setActive("cuatro");
+    if (e.currentTarget.className.split(" ")[0] == "cinco") setActive("cinco");
+  };
   return (
-    <div className="flex justify-center items-center h-screen overflow-hidden bg-sky-500">
+    <div className="flex justify-center items-center h-screen overflow-hidden bg-gray-400">
       <div className="flex w-screen">
-        <img
-          className="bg-cover bg-center bg-no-repeat h-3/4 rounded-3xl m-3 relative w-1"
-          src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt=""
-        />
-        <img
-          className="bg-cover bg-center bg-no-repeat h-3/4 rounded-3xl m-3 relative w-1"
-          src="https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt=""
-        />
-        <img
-          className="bg-cover bg-center bg-no-repeat h-3/4 rounded-3xl m-3 relative w-1"
-          src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
-          alt=""
-        />
-        <img
-          className="bg-cover bg-center bg-no-repeat h-3/4 rounded-3xl m-3 relative w-1"
-          src="https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
-          alt=""
-        />
-        <img
-          className="bg-cover bg-center bg-no-repeat h-3/4 rounded-3xl m-3 relative w-1"
-          src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          alt=""
-        />
+        <div
+          onClick={handleActive}
+          className={`uno bg-cover bg-center bg-no-repeat relative flex m-3 duration-700 ${
+            active == "uno" && "active"
+          }`}
+        >
+          <h3
+            className={`absolute bottom-6 left-6 text-white ${
+              active !== "uno" && "opacity-0"
+            }`}
+          >
+            Explore The World
+          </h3>
+        </div>
+        <div
+          onClick={handleActive}
+          className={`dos bg-cover bg-center bg-no-repeat relative flex m-3 duration-700 ${
+            active == "dos" && "active"
+          }`}
+        >
+          <h3
+            className={`absolute bottom-6 left-6 text-white ${
+              active !== "dos" && "opacity-0"
+            }`}
+          >
+            Wild Forest
+          </h3>
+        </div>
+        <div
+          onClick={handleActive}
+          className={`tres bg-cover bg-center bg-no-repeat relative flex m-3 duration-700 ${
+            active == "tres" && "active"
+          }`}
+        >
+          <h3
+            className={`absolute bottom-6 left-6 text-white ${
+              active !== "tres" && "opacity-0"
+            }`}
+          >
+            Sunny Beach
+          </h3>
+        </div>
+        <div
+          onClick={handleActive}
+          className={`cuatro bg-cover bg-center bg-no-repeat relative flex m-3 duration-700 ${
+            active == "cuatro" && "active"
+          }`}
+        >
+          <h3
+            className={`absolute bottom-6 left-6 text-white ${
+              active !== "cuatro" && "opacity-0"
+            }`}
+          >
+            City on Winter
+          </h3>
+        </div>
+        <div
+          onClick={handleActive}
+          className={`cinco bg-cover bg-center bg-no-repeat relative flex m-3 duration-700 ${
+            active == "cinco" && "active"
+          }`}
+        >
+          <h3
+            className={`absolute bottom-6 left-6 text-white ${
+              active !== "cinco" && "opacity-0"
+            }`}
+          >
+            Mountains - Cloud
+          </h3>
+        </div>
       </div>
     </div>
   );
